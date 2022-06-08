aa6461098a05937f2fae5489e562cc0bc41233f5
Ryze3606 <vincenzosell200010@gmail.com>
2022-06-08 16:43:31 +0200

    token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] })
 
-client.login("OTY5MTk3MTA0NzEzMDYwMzk0.GCvsgh.32cd3EIjUfBi_SsumaSn9nm7muO627JusG_VXU")
+client.login(process.env.token)
 
 
 
